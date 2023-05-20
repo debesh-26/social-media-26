d063a9c4fc37e00fed4a936f6f71cdd99b7c3e05
debesh-26 <dmohapatra291@gmail.com>
2023-05-20 22:30:32 +0530

    changes

--- SocialMedia/react-socialmedia-app/src/config.js
+++ SocialMedia/react-socialmedia-app/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://social-media-26-production.up.railway.app/api",
+    baseURL: "https://social-media-26-production.up.railway.app/api/",
 });
