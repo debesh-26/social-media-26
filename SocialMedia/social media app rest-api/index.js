fe75178bf6f2d7da8a0270b9e7c572cea46fd026
debesh-26 <dmohapatra291@gmail.com>
2023-05-21 11:42:00 +0530

    changes

--- SocialMedia/social media app rest-api/index.js
+++ SocialMedia/social media app rest-api/index.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://kuksocio.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
